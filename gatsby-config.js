92524205f6adb799d8a81af3350ee7db03856099
Daniel Baker <bakerdude@gmail.com>
2020-10-12 12:40:19 +0200

    auth flow

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 /**
  * Configure your Gatsby site with this file.
  *
@@ -6,5 +8,16 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-theme-material-ui`],
-};
+  plugins: [
+    `gatsby-theme-material-ui`,
+    {
+      resolve: `gatsby-plugin-google-gapi`,
+      options: {
+        apiKey: process.env.API_KEY,
+        clientId: process.env.CLIENT_ID,
+        discoveryURLs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+        scopes: ["https://www.googleapis.com/auth/calendar.readonly"],
+      },
+    },
+  ],
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
+
+import { useAuthStatus, loginUser, logoutUser } from "gatsby-plugin-google-gapi"
 
 import { makeStyles } from "@material-ui/core/styles"
 import Box from "@material-ui/core/Box"
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
-import Typography  from "@material-ui/core/Typography"
+import Typography from "@material-ui/core/Typography"
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: "grid",
     placeItems: "center",
-    height: "90vh"
+    height: "90vh",
   },
   card: {
     height: "100%",
@@ -23,17 +25,43 @@ const useStyles = makeStyles(theme => ({
 
 export default function Home() {
   const classes = useStyles()
-  return (
-    <Box className={classes.root}>
-      <Box className={classes.card}>
-        <Typography variant="h2">What's your highlight of the day?</Typography>
-        <Typography variant="body1">
-          Write down what you really want to get done today. This will be your
-          focus.
-        </Typography>
-        <TextField variant="filled" multiline rows={6} defaultValue="Write your highlight here..." />
-        <Button>Next Step</Button>
+
+  const [highlight, setHighlight] = useState()
+  const handleHighlightChangge = event => {
+    setHighlight(event.target.value)
+  }
+
+  const { authed} = useAuthStatus()
+
+  if (authed) {
+    return (
+      <Box className={classes.root}>
+        <Box className={classes.card}>
+          <Typography variant="h2">
+            What's your highlight of the day?
+          </Typography>
+          <Typography variant="body1">
+            Write down what you really want to get done today. This will be your
+            focus.
+          </Typography>
+          <TextField
+            variant="filled"
+            multiline
+            rows={6}
+            defaultValue="Write your highlight here..."
+            onChange={handleHighlightChangge}
+          />
+          <Button disabled={highlight ? false : true}>Next Step</Button>
+          
+          <Button onClick={x => logoutUser(x)}>logOut</Button>
+        </Box>
       </Box>
-    </Box>
-  )
+    )
+  } else {
+    return (
+      <Button variant="contained" color="primary" onClick={loginUser}>
+        login
+      </Button>
+    )
+  }
 }
