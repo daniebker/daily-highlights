e8f529abfad61d0efc7f21e00ee6275642e1918b
Daniel Baker <bakerdude@gmail.com>
2020-10-12 10:05:53 +0200

    section 1 WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,39 @@
 import React from "react"
 
+import { makeStyles } from "@material-ui/core/styles"
+import Box from "@material-ui/core/Box"
+import Button from "@material-ui/core/Button"
+import TextField from "@material-ui/core/TextField"
+import Typography  from "@material-ui/core/Typography"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "grid",
+    placeItems: "center",
+    height: "90vh"
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    padding: "1rem",
+    justifyContent: "space-between",
+  },
+}))
+
 export default function Home() {
-  return <div>Hello world!</div>
+  const classes = useStyles()
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.card}>
+        <Typography variant="h2">What's your highlight of the day?</Typography>
+        <Typography variant="body1">
+          Write down what you really want to get done today. This will be your
+          focus.
+        </Typography>
+        <TextField variant="filled" multiline rows={6} defaultValue="Write your highlight here..." />
+        <Button>Next Step</Button>
+      </Box>
+    </Box>
+  )
 }
