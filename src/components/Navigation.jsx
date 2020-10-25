7709d7e0c9675debc4d98342a908b5188fd225b5
Daniel Baker <bakerdude@gmail.com>
2020-10-25 20:49:13 +0100

    add settings menu button

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,35 +1,36 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Drawer from "@material-ui/core/Drawer"
+import Button from "@material-ui/core/Button"
+import List from "@material-ui/core/List"
+import SettingsIcon from '@material-ui/icons/Settings';
+import ListItem from "@material-ui/core/ListItem"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
+import ListItemText from "@material-ui/core/ListItemText"
 
 const useStyles = makeStyles({
   list: {
     width: 250,
   },
   fullList: {
-    width: 'auto',
+    width: "auto",
   },
-});
+})
 
 export default function TemporaryDrawer() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
 
-  const toggleDrawer = () => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
+  const toggleDrawer = () => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return
     }
 
-    setOpen( !open );
-  };
+    setOpen(!open)
+  }
 
   const list = () => (
     <div
@@ -39,33 +40,22 @@ export default function TemporaryDrawer() {
       onKeyDown={toggleDrawer()}
     >
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem button>
+          <ListItemIcon>
+              <SettingsIcon /> 
+          </ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItem>
       </List>
     </div>
-  );
+  )
 
   return (
     <div>
-      
-          <Button onClick={toggleDrawer()}>Open</Button>
-          <Drawer  open={open} onClose={toggleDrawer()}>
-            {list()}
-          </Drawer>
-      
+      <Button onClick={toggleDrawer()}>Menu</Button>
+      <Drawer open={open} onClose={toggleDrawer()}>
+        {list()}
+      </Drawer>
     </div>
-  );
+  )
 }
